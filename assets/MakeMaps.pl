f040b8637948bfecf83b41addbb83722e6f87427
Vlad <alzwded@gmail.com>
2014-04-01 22:21:20 +0300

    add dies

--- assets/MakeMaps.pl
+++ assets/MakeMaps.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl -w
 
-open A, "<map.idx";
-open F, ">mapdata.js";
+open A, "<map.idx" or die 'cannot read map.idx';
+open F, ">mapdata.js" or die 'cannot open mapdata.js';
 
-print F "var __mapdata = new Array()\n";
+print F "var __mapdata = new Array()\n" or die 'cannot write mapdata.js';
 
 while(<A>) {
-    print F "__mapdata.push(new Array(\n";
     chomp;
     open (my $map, $_) or die("cannot $_");
+    print F "__mapdata.push(new Array(\n";
 
     while(<$map>) {
         chomp;
